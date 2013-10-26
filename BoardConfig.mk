4601ecb8c6c4e578abb13605ade1558340007f1c
martincz <martincz@mokeetek.com>
2013-10-27 03:36:31 +0800

    Code Cleanup
    Change-Id: I24fe34d7fcb43ae5f8d2abfdbcb7a926b68e4753

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,3 @@ TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/i9100g/releasetool
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/i9100g/BoardConfigVendor.mk
 
-# Odex if official release
-ifdef MK_RELEASE
-WITH_DEXPREOPT := true
-endif
